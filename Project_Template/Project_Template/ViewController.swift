93b70d6dd38acacd24a393a4184873b554d186ef
JungsuYeo <duwjdtn@ajou.ac.kr>
2021-07-09 20:46:09 +0900

    프로젝트 템플릿 생성

---
+++ Project_Template/Project_Template/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project_Template
+//
+//  Created by 여정수 on 2021/07/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
